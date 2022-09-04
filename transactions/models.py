6dbad0106aa30bba71e7cec88c65b05dc8ba7abb
Diego <Diegocortes079@gmail.com>
2022-09-03 20:43:01 -0500

    Add transactions model

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Sale(models.Model):
+    wallet_to = models.CharField(max_length=200)
+    timestamp = models.PositiveBigIntegerField(default=0)
+    transaction_id = models.CharField(max_length=200)
+    price_sale = models.DecimalField(max_digits=24, decimal_places=18)
+    quantity = models.PositiveSmallIntegerField(default=1)
