7e987c387afd9462d496dfc5f2ba5a17458b7432
Diego <Diegocortes079@gmail.com>
2022-09-03 20:43:22 -0500

    Add transaction model to admin

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from transactions.models import Sale
+
 # Register your models here.
+
+
+class SaleAdmin(admin.ModelAdmin):
+    list_display = (
+        'wallet_to',
+        'timestamp',
+        'price_sale',
+        'quantity',
+        'transaction_id'
+    )
+    list_filter = (
+        'wallet_to',
+        'timestamp',
+        'price_sale',
+        'quantity',
+        'transaction_id'
+    )
+
+
+admin.site.register(Sale, SaleAdmin)
