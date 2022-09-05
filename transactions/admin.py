4834a3e648b31eaab861e796241ac9b2d737276e
Diego Cortes <diegocortes@Diegos-MacBook-Air.local>
2022-09-04 20:04:45 -0500

    Add button to etherscan

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from transactions.models import Sale
 
@@ -7,19 +9,33 @@ from transactions.models import Sale
 
 class SaleAdmin(admin.ModelAdmin):
     list_display = (
-        'wallet_to',
-        'timestamp',
+        'wallet',
+        'transaction_date',
         'price_sale',
         'quantity',
-        'transaction_id'
+        'hash',
+        'go_to_etherscan'
     )
     list_filter = (
         'wallet_to',
-        'timestamp',
         'price_sale',
         'quantity',
         'transaction_id'
     )
+
+    def wallet(self, obj):
+        return f'{obj.wallet_to[:6]}...{obj.wallet_to[-6:]}'
+
+    def hash(self, obj):
+        return f'{obj.transaction_id[:6]}...{obj.transaction_id[-6:]}'
+
+    def transaction_date(self, obj):
+        return datetime.fromtimestamp(obj.timestamp)
+
+    def go_to_etherscan(self, obj):
+        return mark_safe(
+            f'<a href="https://etherscan.io/tx/{obj.transaction_id}" style="background-color: #1d6aa7; padding: 5px 25px; border-radius: 4px; color: white; " >EtherScan</a>'
+        )
 
 
 admin.site.register(Sale, SaleAdmin)
