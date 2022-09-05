c91a75c55b41faa6e8bb1536567659e824b0aee1
Diego Cortes <diegocortes079@gmail.com>
2022-09-04 20:11:38 -0500

    Create transaction view
    Add transaction view

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from typing import List
+
+from transactions.models import Sale
+from transactions.serializers import SaleSerializer
 
 # Create your views here.
+
+class SaleViewSet(viewsets.ModelViewSet):
+    queryset = Sale.objects.all()
+    serializer_class = SaleSerializer
+    http_method_names: List[str] = ['get', 'post']
